a47da20496c76e49b47725ce3ae912340f128130
tffmh@protonmail.com <tffmh@protonmail.com>
2019-09-21 16:37:21 +0200

    Added all files from the old siteconf, siteconf was in working order.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# site-ffrgw
-Freifunk RGW site configurations 
+# Firmware for Freifunk westliches Ruhrgebiet
+Repository holds the site configuration to build firmware for the Community
+"Freifunk westliches Ruhrgebiet", the configuration is not officially
+sanctioned by "Freifunk westliches Ruhrgebiet e.V.".
+
+## Building the firmware
+
+### Installing dependencys
+As with Debian 10.1
+
+`sudo apt install git subversion build-essential gawk unzip libncurses5-dev zlib1g-dev libssl-dev time`
+
---
+++ site.mk
@@ -0,0 +1,46 @@
+GLUON_SITE_PACKAGES := \
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-mesh-batman-adv-15 \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-radvd \
+	gluon-status-page \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-geo-location-osm \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
+	gluon-web-wifi-config \
+	gluon-web-osm \
+	haveged \
+	iwinfo
+
+# SSID Changer
+GLUON_SITE_PACKAGES += \
+	gluon-ssid-changer
+
+# Languages to include
+GLUON_LANGS ?= en de
+
+DEFAULT_GLUON_RELEASE := 0.10.0a4b2+$(shell date '+%Y%m%d')
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Default priority for updates.
+GLUON_PRIORITY ?= 0
+
+# Region code required for some images; supported values: us eu
+GLUON_REGION ?= eu
+
+# Select ath10k Firmware for adhoc
+# GLUON_ATH10K_MESH ?= 11s
+
+# Brach for Autoupdate
+GLUON_BRANCH ?= experimental
